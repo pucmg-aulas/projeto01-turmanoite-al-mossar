95660b02c1e74393f588e5adc045c9041f32d588
GabrielBurdignon <108158236+GabrielBurdignon@users.noreply.github.com>
2024-04-21 22:06:50 -0300

    Update FilaDeEspera.java
    atualização

--- codigo/FilaDeEspera.java
+++ codigo/FilaDeEspera.java
@@ -1,14 +1,37 @@
-import java.util.Date;
-import java.util.List;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class FilaDeEspera {
-    private List<Cliente> clientes;
+    private Queue<Cliente> clientes;
+
+
+    public FilaDeEspera() {
+        this.clientes = new LinkedList<>();
+    }
 
     public void adicionarCliente(Cliente cliente) {
-
+        clientes.add(cliente);
     }
 
     public void removerCliente(Cliente cliente) {
-
+        clientes.remove(cliente);
     }
+
+    public Queue<Cliente> getFila() {
+        return clientes;
+    }
+
+    public boolean estaVazia() {
+        return clientes.isEmpty();
+    }
+
+    public Cliente proximoCliente() {
+        return clientes.poll();
+    }
+
+    public Cliente removerCliente() {
+        return clientes.poll();
+    }
+
+
 }
