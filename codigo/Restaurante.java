74e89fd95735698a5861ba46031823112c2d7420
GabrielBurdignon <108158236+GabrielBurdignon@users.noreply.github.com>
2024-04-21 22:10:22 -0300

    Update Restaurante.java
    atualização

--- codigo/Restaurante.java
+++ codigo/Restaurante.java
@@ -1,12 +1,49 @@
 import java.util.Date;
 import java.util.List;
+import java.util.ArrayList;
 
 public class Restaurante {
-    private int totalMesas;
     private List<Mesa> mesas;
-    private FilaDeEspera fila;
+    private FilaDeEspera filaDeEspera;
 
-    public void alocarMesa(Cliente cliente) {
-
+    public Restaurante() {
+        this.mesas = new ArrayList<>();
+        for (int i = 0; i < 4; i++) this.mesas.add(new Mesa(i + 1, 4));
+        for (int i = 4; i < 8; i++) this.mesas.add(new Mesa(i + 1, 6));
+        for (int i = 8; i < 10; i++) this.mesas.add(new Mesa(i + 1, 8));
+        this.filaDeEspera = new FilaDeEspera();
     }
+
+    public void processarRequisicaoCliente(Cliente cliente) {
+        Mesa mesaDisponivel = encontrarMesaDisponivel(cliente.getNumPessoas());
+        if (mesaDisponivel != null) {
+            mesaDisponivel.ocupar();
+            Requisicao requisicao = new Requisicao();
+            requisicao.setMesa(mesaDisponivel);
+            requisicao.registrarEntrada(new Date());
+            cliente.setRequisicao(requisicao);
+            System.out.println("Mesa " + mesaDisponivel.getId() + " (capacidade " + mesaDisponivel.getCapacidade() + ") alocada ao cliente " + cliente.getNome());
+        } else {
+            System.out.println("Todas as mesas adequadas estão ocupadas. Cliente " + cliente.getNome() + " adicionado à fila de espera.");
+            filaDeEspera.adicionarCliente(cliente);
+        }
+    }
+
+    private Mesa encontrarMesaDisponivel(int numPessoas) {
+        for (Mesa mesa : mesas) {
+            if (!mesa.isOcupada() && mesa.getCapacidade() >= numPessoas) {
+                return mesa;
+            }
+        }
+        return null;
+    }
+
+    public List<Mesa> getMesas() {
+        return mesas;
+    }
+
+    public FilaDeEspera getFilaDeEspera() {
+        return filaDeEspera;
+    }
+
 }
