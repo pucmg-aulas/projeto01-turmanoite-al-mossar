415d51081f3ac7730b20b0cf5bd828b9abee25e0
GabrielBurdignon <108158236+GabrielBurdignon@users.noreply.github.com>
2024-04-21 22:06:06 -0300

    Update Cliente.java
    atualização

--- codigo/Cliente.java
+++ codigo/Cliente.java
@@ -1,17 +1,39 @@
 import java.util.Date;
-import java.util.List;
 
 public class Cliente {
-    private String nome;
-    private int numPessoas;
-    private Date dataEntrada;
-    private Date dataSaida;
+  private String nome;
+  private int numPessoas;
+  private Requisicao requisicao;
 
-    public void checkIn(Date hora) {
+  public Cliente(String nome, int numPessoas) {
+    this.nome = nome;
+    this.numPessoas = numPessoas;
+  }
 
+  public String getNome() {
+    return nome;
+  }
+
+  public int getNumPessoas() {
+    return numPessoas;
+  }
+
+  public Requisicao getRequisicao() {
+    return requisicao;
+  }
+
+  public void setRequisicao(Requisicao requisicao) {
+    this.requisicao = requisicao;
+  }
+
+  public void sairDoRestaurante() {
+    if (this.requisicao != null && this.requisicao.getMesa() != null) {
+      this.requisicao.getMesa().desocupar();
+      this.requisicao.registrarSaida(new Date());
+      System.out.println(
+          "Cliente " + nome + " concluiu sua refeição e desocupou a mesa " + this.requisicao.getMesa().getId() + ".");
+    } else {
+      System.out.println("Cliente " + nome + " não está em uma mesa.");
     }
-
-    public void checkOut(Date hora) {
-
-    }
+  }
 }
