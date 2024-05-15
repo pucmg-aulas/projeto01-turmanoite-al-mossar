d15d28f8506d7d56154d9fe5d2960504220505b1
Vinicius Augusto Ribeiro Mazzoli <100352824+viniciusmazzoli@users.noreply.github.com>
2024-05-14 21:08:41 -0300

    Update Mesa.java

--- codigo/Mesa.java
+++ codigo/Mesa.java
@@ -2,14 +2,25 @@ public class Mesa {
     private int id;
     private int capacidade;
     private boolean ocupada;
-    private Cliente cliente; 
+    private Cliente cliente;
+    private Pedido pedido;
 
     public Mesa(int id, int capacidade) {
         this.id = id;
         this.capacidade = capacidade;
         this.ocupada = false;
         this.cliente = null;
+        this.pedido = null;
     }
+
+    public void iniciarPedido() {
+            this.pedido = new Pedido();
+        }
+
+        public Pedido getPedido() {
+            return pedido;
+        }
+    
 
     public int getId() {
         return id;
@@ -23,13 +34,15 @@ public class Mesa {
         return ocupada;
     }
 
-    public void ocupar() {
-        ocupada = true;
+    public void ocupar(Cliente cliente) {
+        this.ocupada = true;
+        this.cliente = cliente;
+        this.pedido = new Pedido();  // Inicia um novo pedido assim que a mesa é ocupada
     }
 
     public void desocupar() {
         ocupada = false;
-        cliente = null; 
+        cliente = null;  // Limpar referência ao cliente ao desocupar
     }
 
     public Cliente getCliente() {
