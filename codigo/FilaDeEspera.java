e56eb1010aecd538b1df0ed696d2a79b8888339d
guilherme_silveira <91088737+guilhermsilveira@users.noreply.github.com>
2024-05-21 18:33:20 -0300

    upload uml 5 - rename files

--- codigo/FilaDeEspera.java
+++ codigo/FilaDeEspera.java
@@ -39,45 +39,4 @@ public class FilaDeEspera {
   }
 
 
-}import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Iterator;
-
-public class FilaDeEspera {
-    private Queue<Cliente> clientes;
-
-
-    public FilaDeEspera() {
-        this.clientes = new LinkedList<>();
-    }
-
-    public void adicionarCliente(Cliente cliente) {
-        clientes.add(cliente);
-    }
-
-
-    public Queue<Cliente> getFila() {
-        return clientes;
-    }
-
-    public boolean estaVazia() {
-        return clientes.isEmpty();
-    }
-
-    public Cliente proximoCliente() {
-        return clientes.poll();
-    }
-
-
-  public void removerCliente(Cliente cliente) {
-      Iterator<Cliente> iterator = clientes.iterator();
-      while (iterator.hasNext()) {
-          if (iterator.next().equals(cliente)) {
-              iterator.remove();
-              break;
-          }
-      }
-  }
-
-
 }
